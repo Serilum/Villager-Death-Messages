2938854828ebd1e02b6a30a3bb4ce4accefd02a3
Rick South <rick@natamus.com>
2024-06-14 05:06:35 +0200

    Villager Death Messages source code for 1.21.0.

---
+++ NeoForge/src/main/java/com/natamus/villagerdeathmessages/neoforge/events/NeoForgeVillagerEvent.java
@@ -0,0 +1,16 @@
+package com.natamus.villagerdeathmessages.neoforge.events;
+
+import com.natamus.villagerdeathmessages.events.VillagerEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeVillagerEvent {
+	@SubscribeEvent
+	public static void villagerDeath(LivingDeathEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		VillagerEvent.villagerDeath(livingEntity.level(), livingEntity, e.getSource());
+	}
+}
