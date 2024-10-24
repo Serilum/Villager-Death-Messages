296c04bb565190ccf998bd176bab0536f2787fe7
Rick South <rick@natamus.com>
2024-10-24 17:55:23 +0200

    Villager Death Messages source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/villagerdeathmessages/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.villagerdeathmessages.util;
+
+public class Reference {
+	public static final String MOD_ID = "villagerdeathmessages";
+	public static final String NAME = "Villager Death Messages";
+	public static final String VERSION = "3.5";
+	public static final String ACCEPTED_VERSIONS = "[1.21.2]";
+}
