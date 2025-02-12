fd485b5e33541be470058b46c6d7573687188d59
Rick South <rick@natamus.com>
2025-02-12 23:08:48 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/villagerdeathmessages/forge/events/ForgeVillagerEvent.java
+++ Forge/src/main/java/com/natamus/villagerdeathmessages/forge/events/ForgeVillagerEvent.java
@@ -4,12 +4,10 @@ import com.natamus.villagerdeathmessages.events.VillagerEvent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeVillagerEvent {
 	@SubscribeEvent
-	public void villagerDeath(LivingDeathEvent e) {
+	public static void villagerDeath(LivingDeathEvent e) {
 		LivingEntity livingEntity = e.getEntity();
 		VillagerEvent.villagerDeath(livingEntity.level(), livingEntity, e.getSource());
 	}
--- NeoForge/src/main/java/com/natamus/villagerdeathmessages/neoforge/events/NeoForgeVillagerEvent.java
+++ NeoForge/src/main/java/com/natamus/villagerdeathmessages/neoforge/events/NeoForgeVillagerEvent.java
@@ -4,9 +4,7 @@ import com.natamus.villagerdeathmessages.events.VillagerEvent;
 import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeVillagerEvent {
 	@SubscribeEvent
 	public static void villagerDeath(LivingDeathEvent e) {
