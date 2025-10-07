0947e83fafe87ff6fda7ff5a57c27d1b336caf8b
Rick South <rick@natamus.com>
2025-10-08 00:49:35 +0200

    Villager Death Messages source code for 1.21.10.

---
+++ Common/src/main/java/com/natamus/villagerdeathmessages/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.villagerdeathmessages;
+
+import com.natamus.villagerdeathmessages.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
