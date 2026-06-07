f2cc9cdca60c7521b3fd18cd4dd25e003676d58c
Rick South <38502394+ricksouth@users.noreply.github.com>
2026-06-07 19:30:32 +0200

    (v3.9) Updated the mod's text to use Collective's new translation system, which adds many languages alongside English! [+1]

--- Common/src/main/java/com/natamus/villagerdeathmessages/util/Reference.java
+++ Common/src/main/java/com/natamus/villagerdeathmessages/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.villagerdeathmessages.util;
 public class Reference {
 	public static final String MOD_ID = "villagerdeathmessages";
 	public static final String NAME = "Villager Death Messages";
-	public static final String VERSION = "3.8";
+	public static final String VERSION = "3.9";
 	public static final String ACCEPTED_VERSIONS = "[1.21.11]";
 }
